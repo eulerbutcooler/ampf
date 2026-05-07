40495819d3e1e360897a337e35e95c2ce896dbce
eulerbutcooler <eulerbutcooler@gmail.com>
2026-05-07 23:05:03 +0530

    Removed cloudflare workers config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    imageService: "cloudflare"
-  }),
-
   integrations: [mdx()]
 });
